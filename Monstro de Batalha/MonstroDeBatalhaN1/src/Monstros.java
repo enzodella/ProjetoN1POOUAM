430bf8b3a76a2bcbaa312d15a5680c8c5d7b0d42
enzodella <83982785+enzodella@users.noreply.github.com>
2021-05-10 18:27:35 -0300

    Update Monstros.java
    Criação dos atributos e do método construtor da classe abstrata monstros e a implementação dos dados dos monstros utilizados no game

--- Monstro de Batalha/MonstroDeBatalhaN1/src/Monstros.java
+++ Monstro de Batalha/MonstroDeBatalhaN1/src/Monstros.java
@@ -1,3 +1,23 @@
-public class Monstros {
-    
+public abstract class Monstros {
+    protected String nome[] = new String[6];
+    protected String elemento[] = new String[6];
+    protected int vida[] = new int[6];
+    protected int forca[] = new int[6];
+    protected int defesa[] = new int[6];
+    protected int velocidade[] = new int[6];
+
+    protected Monstros(){
+        String name[] = new String[]{"Oshuo", "Elco", "Nurion","Plilu", "Cisur","Cubaytou"};
+        this.nome = name;
+        String element[] = new String[]{"Fogo", "Grama", "Agua","Luz", "Gelo","Escuridao"};
+        this.elemento = element;
+        int health[] = new int[]{110, 110, 90, 90, 100, 90};
+        this.vida = health;
+        int strength[] = new int[]{50, 60, 50, 50, 50, 40};
+        this.forca = strength;
+        int defense[] = new int[]{15, 15, 10, 10, 10, 10};
+        this.defesa = defense;
+        int velocity[] = new int[]{20, 15, 25, 30, 15, 30};
+        this.velocidade = velocity;
+    }
 }
