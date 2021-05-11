123278e0a4b2526de8147ebef4bfde0875252b51
Felipe de Oliveira Machado <77800331+FelipeOliveiraM18@users.noreply.github.com>
2021-05-11 17:30:27 -0300

    Update Acoes.java
    Correção de erro da validação da seleção de monstros.

--- Monstro de Batalha/MonstroDeBatalhaN1/src/Acoes.java
+++ Monstro de Batalha/MonstroDeBatalhaN1/src/Acoes.java
@@ -40,118 +40,126 @@ public class Acoes{
         int respMenu = 0, x = 0;
         boolean teste = false;
         do {
-            System.out.println("Jogador " + jogador.numPlayer + ", escolha seus monstros!");
-            System.out.println("1 - Oshuo, Elemento: Fogo, Vida: 110, Força 50, Defesa: 15, Velocidade: 20\n"
-                    + "2 - Elco, Elemento: Grama, Vida: 110, Força 60, Defesa: 15, Velocidade: 15 \n"
-                    + "3 - Nurion, Elemento: Agua, Vida: 90, Força 50, Defesa: 10, Velocidade: 25 \n"
-                    + "4 - Plilu, Elemento: Luz, Vida: 90, Força 50, Defesa: 10, Velocidade: 15 \n"
-                    + "5 - Cisur, Elemento: Gelo, Vida: 100, Força 50, Defesa: 10, Velocidade: 15 \n"
-                    + "6 - Cubaytou, Elemento: Escuridão, Vida: 80, Força 40, Defesa: 10, Velocidade: 30 \n"
-                    + "9 - Sair");
-            respMenu = ler.nextInt();
-            switch (respMenu) {
-                case 1:
-                    do {
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 0) {
-                                teste = true;
+            do {
+                System.out.println("Jogador " + jogador.numPlayer + ", escolha seus monstros!");
+                System.out.println("1 - Oshuo, Elemento: Fogo, Vida: 110, Força 50, Defesa: 15, Velocidade: 20\n"
+                        + "2 - Elco, Elemento: Grama, Vida: 110, Força 60, Defesa: 15, Velocidade: 15 \n"
+                        + "3 - Nurion, Elemento: Agua, Vida: 90, Força 50, Defesa: 10, Velocidade: 25 \n"
+                        + "4 - Plilu, Elemento: Luz, Vida: 90, Força 50, Defesa: 10, Velocidade: 15 \n"
+                        + "5 - Cisur, Elemento: Gelo, Vida: 100, Força 50, Defesa: 10, Velocidade: 15 \n"
+                        + "6 - Cubaytou, Elemento: Escuridão, Vida: 80, Força 40, Defesa: 10, Velocidade: 30 \n"
+                        + "9 - Sair");
+                respMenu = ler.nextInt();
+                switch (respMenu) {
+                    case 1:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 0) {
+                                    teste = true;
+                                }
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 0;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 2:
-                    do {
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 1) {
-                                teste = true;
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 0;
+                                x++;
+                                teste = false;
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 1;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 3:
-                     do{
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 2) {
-                                teste = true;
+                        } while (teste == true);
+                        break;
+                    case 2:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 1) {
+                                    teste = true;
+                                }
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 2;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 4:
-                    do {
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 3) {
-                                teste = true;
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 1;
+                                x++;
+                                teste = false;
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 3;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 5:
-                    do{
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 4) {
-                                teste = true;
+                        } while (teste == true);
+                        break;
+                    case 3:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 2) {
+                                    teste = true;
+                                }
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 5;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 6:
-                    do {
-                        for (int c = 0; c < 3; c++) {
-                            if (jogador.monstros[c] == 0) {
-                                teste = true;
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 2;
+                                x++;
+                                teste = false;
                             }
-                        }
-                        if (teste == true) {
-                            System.out.println("Monstro já selecionado anteriormente!!");
-                        } else {
-                            jogador.monstros[x] = 6;
-                            x++;
-                            teste = false;
-                        }
-                    }while (teste == true);
-                    break;
-                case 9:
-                    break;
-                default:
-                    System.out.println("Entrada inválida!");
-                    break;
-            }
+                        } while (teste == true);
+                        break;
+                    case 4:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 3) {
+                                    teste = true;
+                                }
+                            }
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 3;
+                                x++;
+                                teste = false;
+                            }
+                        } while (teste == true);
+                        break;
+                    case 5:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 4) {
+                                    teste = true;
+                                }
+                            }
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 4;
+                                x++;
+                                teste = false;
+                            }
+                        } while (teste == true);
+                        break;
+                    case 6:
+                        do {
+                            for (int c = 0; c < 3; c++) {
+                                if (jogador.monstros[c] == 5) {
+                                    teste = true;
+                                }
+                            }
+                            if (teste == true) {
+                                System.out.println("Monstro já selecionado anteriormente!!");
+                                teste = false;
+                            } else {
+                                jogador.monstros[x] = 5;
+                                x++;
+                                teste = false;
+                            }
+                        } while (teste == true);
+                        break;
+                    case 9:
+                        break;
+                    default:
+                        System.out.println("Entrada inválida!");
+                        break;
+                }
+            } while (teste == true);
         } while (x < 3 && respMenu != 9);
 
         Thread.sleep(2000);
